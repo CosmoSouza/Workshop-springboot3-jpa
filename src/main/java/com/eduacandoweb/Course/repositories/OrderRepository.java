17141c82e699c9c225dab13b80bfdb5ae0b82bf6
CosmoSouza <lucascap9@gmail.com>
2025-07-11 16:37:05 -0300

    Order, Instant, ISO 8601 PART2

---
+++ src/main/java/com/eduacandoweb/Course/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.eduacandoweb.Course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.eduacandoweb.Course.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
