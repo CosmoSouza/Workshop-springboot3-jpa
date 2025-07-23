171f7cc584374f8d22a564de20dbcfa162a9d06f
CosmoSouza <lucascap9@gmail.com>
2025-07-23 15:38:21 -0300

    Exception handling - findByid

--- src/main/java/com/eduacandoweb/Course/services/UserService.java
+++ src/main/java/com/eduacandoweb/Course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.eduacandoweb.Course.entities.User;
 import com.eduacandoweb.Course.repositories.UserRepository;
+import com.eduacandoweb.Course.services.exceptions.ResourceNotFoundException;
 
 @Service	
 public class UserService {
@@ -22,7 +23,7 @@ public class UserService {
 		
 		public User findById(Long id) {
 			Optional<User> obj = repository.findById(id);
-			return obj.get();
+			return obj.orElseThrow(()-> new ResourceNotFoundException(id));
 		}
 		
 		public User insert(User obj) {
---
+++ src/main/java/com/eduacandoweb/Course/services/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.eduacandoweb.Course.services.exceptions;
+
+import java.time.Instant;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.eduacandoweb.Course.resources.exceptions.StandardError;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest resquest) {
+		String error = "Resource not found";
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), resquest.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+		
+	}
+
+}
