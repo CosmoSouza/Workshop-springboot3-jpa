dabc49176047be905846151ddcc7230e9e4dfdd0
CosmoSouza <lucascap9@gmail.com>
2025-07-23 10:59:32 -0300

    Payment, one-to-one association

--- src/main/java/com/eduacandoweb/Course/TestConfig.java
+++ src/main/java/com/eduacandoweb/Course/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.eduacandoweb.Course.entities.Category;
 import com.eduacandoweb.Course.entities.Order;
 import com.eduacandoweb.Course.entities.OrderItem;
+import com.eduacandoweb.Course.entities.Payment;
 import com.eduacandoweb.Course.entities.Product;
 import com.eduacandoweb.Course.entities.User;
 import com.eduacandoweb.Course.entities.enums.OrderStatus;
@@ -84,6 +85,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));	
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 	
 }
